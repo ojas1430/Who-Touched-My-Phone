303b40dfd56ab02ba436fab72922fe4db7f0c456
OJAS-CHOUDHARY <ojas1430@gmail.com>
2026-05-04 13:45:07 +0530

    Add confirm pass and Lock Screens

---
+++ app/src/main/java/com/ojasx/whotouchedmyphone/Password/ConfirmPassword.kt
@@ -0,0 +1,97 @@
+package com.ojasx.whotouchedmyphone.Password
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ojasx.whotouchedmyphone.Password.NumberPad.NumberPad
+import com.ojasx.whotouchedmyphone.Password.NumberPad.PinDots
+import com.ojasx.whotouchedmyphone.R
+
+@Preview
+@Composable
+fun ConfirmPassword() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF0B0E17),
+                        Color(0xFF111528),
+                        Color(0xFF0B0E17)
+                    )
+                ))
+    ) {
+
+        // Top content (60%)
+        Box(
+            modifier = Modifier
+                .weight(0.6f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize()
+                    .padding(horizontal = 8.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.firstlock),
+                    contentDescription = null
+                )
+                Text(
+                    "Confirm Your PIN",
+                    color = Color.White,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold
+
+                )
+                Spacer(Modifier.height(20.dp))
+                Text(
+                    "Please re-enter the same PIN.",
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center   ,
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                )
+                Spacer(Modifier.height(20.dp))
+                PinDots(
+                    pinLength = 2
+                )
+            }
+        }
+
+        // Bottom NumberPad (40%)
+        NumberPad(
+            modifier = Modifier
+                .weight(0.4f)
+                .fillMaxWidth(),
+            onNumberClick = {},
+            onDeleteClick = {},
+            onConfirmClick = {}
+        )
+        Spacer(Modifier.height(20.dp))
+    }
+}
+
---
+++ app/src/main/java/com/ojasx/whotouchedmyphone/Password/LockScreen.kt
@@ -0,0 +1,117 @@
+package com.ojasx.whotouchedmyphone.Password
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ojasx.whotouchedmyphone.Password.NumberPad.NumberPad
+import com.ojasx.whotouchedmyphone.Password.NumberPad.PinDots
+import com.ojasx.whotouchedmyphone.R
+
+@Preview
+@Composable
+fun LockScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF0B0E17),
+                        Color(0xFF111528),
+                        Color(0xFF0B0E17)
+                    )
+                )
+            )
+    ) {
+
+        Box(
+            modifier = Modifier
+                .weight(0.6f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .size(300.dp)
+                    .offset(y = (-40).dp)
+                    .background(
+                        brush = Brush.radialGradient(
+                            colors = listOf(
+                                Color(0xFF7B4DFF).copy(alpha = 0.6f),
+                                Color.Transparent
+                            )
+                        ),
+                        shape = CircleShape
+                    )
+                    .blur(120.dp)
+            )
+
+            Box(
+                modifier = Modifier
+                    .offset(x = (-40).dp, y = (-20).dp)
+                    .background(
+                        brush = Brush.radialGradient(
+                            colors = listOf(
+                                Color(0xFFE9DDFF).copy(alpha = 0.2f),
+                                Color.Transparent
+                            )
+                        ),
+                        shape = CircleShape
+                    )
+                    .blur(120.dp)
+            )
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.lockicon),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .shadow(20.dp, CircleShape)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Enter PIN",
+                    color = Color.White,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                PinDots(pinLength = 2)
+            }
+        }
+
+        NumberPad(
+            modifier = Modifier
+                .weight(0.4f)
+                .fillMaxWidth(),
+            onNumberClick = {},
+            onDeleteClick = {},
+            onConfirmClick = {}
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+    }
+}
